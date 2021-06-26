2481a659deae00f22cf132a9b3dd1c7e2f062bad
Francisco Macuacua <francisco@Franciscos-iMac.local>
2021-06-26 17:12:05 +0200

    MAKE LINK CLICKEBLE

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -9,26 +9,31 @@ class DrawerTile extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return SizedBox(
-      height: 60,
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Icon(
-                iconData,
-              size: 32,
-              color: Colors.grey[700],
-            ),
-          ),
-          Text(
-              title,
-              style: TextStyle(
-                fontSize: 16,
+    return InkWell(
+      onTap: (){
+
+      },
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Icon(
+                  iconData,
+                size: 32,
                 color: Colors.grey[700],
               ),
-          )
-        ],
+            ),
+            Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[700],
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
