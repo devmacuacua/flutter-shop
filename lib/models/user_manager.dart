79aacddcbfe292504981cd2fb8e1c48260301e3e
Francisco Macuacua <francisco@Franciscos-iMac.local>
2021-06-27 05:50:05 +0200

    Validando Erros ao Autenticar

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:store/helpers/firebase_errors.dart';
 import 'package:store/models/user_detail.dart';
 
 class UserManager {
@@ -13,8 +14,18 @@ class UserManager {
         final UserCredential result = await auth.signInWithEmailAndPassword(
             email: user.email, password: user.password);
         print(result.user!.uid);
-      } on PlatformException catch (e){
-        print(e);
+      }
+      on PlatformException catch (e){
+        print(getErrorString(e.code));
+      }
+      on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          print('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          print('Wrong password provided for that user.');
+        }else{
+          print(getErrorString(e.code));
+        }
       }
 
   }
